ed3dd5425db16a4d77c8d884dc7fcb7a49aa75a6
afeiship <1290657123@qq.com>
2019-03-24 16:54:43 +0800

    add: hello rc

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,3 +1,27 @@
 import webapck from 'webpack';
+import { resolve } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-export default (inEnv) => {};
+export default (inEnv) => {
+  console.log(process.argv);
+  return {
+    entry: './src/main.js',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.scss', '.js', '.jsx']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: resolve(__dirname, './public/index.ejs')
+      })
+    ]
+  };
+};
